fa067b99bdd196e48ab6c6b23ae982534239ceba
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-06 23:46:56 +0530

    Alert

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,14 @@
 let express = require("express");
 let router = express.Router();
 
-router.get("/", function (req, res) {});
+router.get("/dashboard", function (req, res) {
+  let userid = req.query.userid,
+    password = req.query.password;
+  if (userid == "1234" && password == "1234") {
+    res.render("dashboard");
+  } else {
+    res.render("login");
+  }
+});
 
 module.exports = router;
