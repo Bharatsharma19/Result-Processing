f2625a755154b1dee36a6ecff30a4e80de9dc0fb
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-07 00:00:58 +0530

    Connect-Flash

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ let router = express.Router();
 router.get("/dashboard", function (req, res) {
   let userid = req.query.userid,
     password = req.query.password;
+
   if (userid == "1234" && password == "1234") {
     res.render("dashboard");
   } else {
